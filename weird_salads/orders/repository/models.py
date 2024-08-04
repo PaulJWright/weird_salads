955788d8b324f7deaacf759c29cf5a1a34fbf2ec
Paul Wright <paul@pauljwright.co.uk>
2024-08-05 00:53:54 +0100

    rename uuid fn to include str_uuid

--- weird_salads/orders/repository/models.py
+++ weird_salads/orders/repository/models.py
@@ -7,7 +7,7 @@ from datetime import datetime, timezone
 from sqlalchemy import Column, DateTime, Integer, String
 
 from weird_salads.utils.sqlalchemy_base import Base
-from weird_salads.utils.utils import generate_uuid
+from weird_salads.utils.utils import generate_str_uuid
 
 __all__ = ["OrderModel"]
 
@@ -15,7 +15,7 @@ __all__ = ["OrderModel"]
 class OrderModel(Base):
     __tablename__ = "order"
 
-    id = Column(String, primary_key=True, default=generate_uuid)
+    id = Column(String, primary_key=True, default=generate_str_uuid)
     menu_id = Column(Integer, nullable=False)  # this will end up being a ForeignKey
     created = Column(DateTime, default=datetime.now(timezone.utc))
 
--- weird_salads/utils/utils.py
+++ weird_salads/utils/utils.py
@@ -1,5 +1,8 @@
 import uuid
 
+__all__ = ["generate_str_uuid"]
 
-def generate_uuid():
+
+def generate_str_uuid():
+    """str(uuid) for sqlite"""
     return str(uuid.uuid4())
