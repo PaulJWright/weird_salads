12d5eee1044fc58617f2095850b10f445d01ca01
Paul Wright <paul@pauljwright.co.uk>
2024-08-04 23:06:07 +0100

    addition of single order table

---
+++ weird_salads/orders/repository/models.py
@@ -0,0 +1,27 @@
+"""
+SQLalchemy Models
+"""
+
+from datetime import datetime, timezone
+
+from sqlalchemy import Column, DateTime, Integer, String
+
+from weird_salads.utils.sqlalchemy_base import Base
+from weird_salads.utils.utils import generate_uuid
+
+__all__ = ["OrderModel"]
+
+
+class OrderModel(Base):
+    __tablename__ = "order"
+
+    id = Column(String, primary_key=True, default=generate_uuid)
+    menu_id = Column(Integer, nullable=False)  # this will end up being a ForeignKey
+    created = Column(DateTime, default=datetime.now(timezone.utc))
+
+    def dict(self):
+        return {
+            "id": self.id,
+            "menu_id": self.menu_id,
+            "created": self.created,
+        }
