40d8651f70a3cd6b3ff1a8188ef18720e9b3ae7a
Paul Wright <paul@pauljwright.co.uk>
2024-08-05 01:15:43 +0100

    order table

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,16 +1,23 @@
+import pandas as pd
 import requests
 import streamlit as st
 
 st.title("Streamlit App")
 
 # Try to get a response from FastAPI
-# now from order
 try:
     response = requests.get("http://fastapi:8000/order/")
     response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
     data = response.json()
 
-    st.write("Response:", data)
+    # Extract orders from the response
+    orders = data.get("orders", [])
+
+    # Convert the list of orders to a DataFrame
+    df = pd.DataFrame(orders)
+
+    # Display the DataFrame as a table
+    st.table(df)
 
 except requests.exceptions.RequestException as e:
     st.write("Failed to connect to FastAPI:", e)
